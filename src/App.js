7ae80f0b8190a5720c01abc8428970463cf18bb8
Akash <akash.0711a@gmail.com>
2021-09-14 19:51:02 +0530

    new changes

--- src/App.js
+++ src/App.js
@@ -1,23 +1,37 @@
-import logo from './logo.svg';
+// import './key/key.js'
+import Axios from 'axios';
 import './App.css';
 
+import { useState } from 'react';
+
 function App() {
+
+  const [ query , setQuery] = useState("")
+    
+  const YOUR_APP_ID = "b1dda41e";
+  const YOUR_APP_KEY = "2a667764448a09bf641f68df28cd3cdd";
+
+  const url = `https://api.edamam.com/search?q=${query}&app_id=${YOUR_APP_ID}&app_key=${YOUR_APP_KEY}
+  &calories=591-722&health=alcohol-free`;
+
+  async function getRecipes()
+  {
+    const result = await Axios.get(url);
+    console.log(result);
+    
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <h1 onClick = {getRecipes}>Recipz 🍚</h1>
+
+     <form className = "App_serchForm">
+       <input
+        type="text"
+        placeholder = "Enter Valid Ingredient"
+        value={query}
+        onChange={(e)=>setQuery(e.target.value)}
+       />
+     </form>
     </div>
   );
 }
---
+++ src/key/key.js
@@ -0,0 +1,2 @@
+const YOUR_APP_ID = "b1dda41e";
+const YOUR_APP_KEY = "2a667764448a09bf641f68df28cd3cdd";
